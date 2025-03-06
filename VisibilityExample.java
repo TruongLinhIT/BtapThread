d999a0b8f960594c87c17362f0c0aeb6626e4e94
TruongLinhIT <linhtvt.24it@vku.udn.vn>
2025-03-06 13:41:53 +0700

    Add files via upload

---
+++ VisibilityExample.java
@@ -0,0 +1,15 @@
+package baitapthread;
+
+public class VisibilityExample {
+	private static volatile boolean flag = false; 
+
+    public static void main(String[] args) {
+        new Thread(() -> {
+            while (!flag) {} 
+            System.out.println("Flag changed!");
+        }).start();
+
+        try { Thread.sleep(2000); } catch (InterruptedException e) {}
+        flag = true; 
+    }
+}
